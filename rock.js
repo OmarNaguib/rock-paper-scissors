858e4ef8298856f1dca5bfad9cb1798c4ceed442
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-11 17:07:21 +0200

    add Playround()

--- rock.js
+++ rock.js
@@ -10,3 +10,32 @@ function getPlayerSelection() {
     return playerSelection
 }
 
+function playRound(playerSelection=getPlayerSelection(),computerChoice=getComputerChoice()) {
+    if (playerSelection === "rock") { 
+
+        if (playerSelection === computerChoice) {return "It's a Draw, both Rocks"}
+        else if (computerChoice === "scissors") {return "You win! Rock beats Scissors"}
+        else if (computerChoice === "paper") {return "You Lose! Paper beats Rock"}
+       }
+    else if (playerSelection === "paper") {  
+
+        if (playerSelection === computerChoice) {return "It's a Draw, Both Papers"}
+        else if (computerChoice === "scissors") {return "You Lose! Scissors beats Paper"}
+        else if (computerChoice === "rock") {return "You Win! Paper beats Rock"}
+
+      }
+    else if (playerSelection === "scissors") {  
+
+        if (playerSelection === computerChoice) {return "It's a Draw, Both Scissors"}
+        else if (computerChoice === "paper") {return "You Win! Scissors beats Paper"}
+        else if (computerChoice === "rock") {return "You Lose! Rock beats Scissors"}
+
+      }
+    else  { return "invalid input" }
+
+
+}
+
+
+result=playRound();
+console.log(result)
